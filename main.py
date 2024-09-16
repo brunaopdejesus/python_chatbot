fba027ec4b91dc49b1aec107443199da843cb873
leoruiz197 <ruizruizleo@hotmail.com>
2024-09-16 16:36:46 -0300

    teste

--- main.py
+++ main.py
@@ -1,54 +1,34 @@
 import os
+from typing import List
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Rota para lidar com as requisições do webhook
-@app.route('/', methods=['GET'])
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "OK", 200
+  return 'OK', 200
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
 
-    # Verifica se há uma callback_query do Telegram
-    if 'callback_query' in req:
-        callback_data = req['callback_query']['data']
-        
-        # Processar o callback_data dos botões do Telegram
-        if callback_data == 'opcao_1':
-            response_text = "Você escolheu a Opção 1."
-        elif callback_data == 'opcao_2':
-            response_text = "Você escolheu a Opção 2."
-        else:
-            response_text = "Desculpe, não entendi sua escolha."
+def format_response(texts: List[str]) -> jsonify:
+  return jsonify({"fulfillmentMessages": [{"text": {"text": texts}}]})
 
-        # Enviar resposta de volta para o Telegram
-        return jsonify({
-            "method": "sendMessage",
-            "chat_id": req['callback_query']['from']['id'],
-            "text": response_text
-        })
-    
-    # Processar intents do Dialogflow, caso não seja callback do Telegram
-    intent_name = req.get('queryResult').get('intent').get('displayName')
-    
-    if intent_name == 'Opção 1 Intent':
-        response_text = "Você escolheu a Opção 1."
-    elif intent_name == 'Opção 2 Intent':
-        response_text = "Você escolheu a Opção 2."
-    else:
-        response_text = "Desculpe, não entendi sua escolha."
 
-    return make_webhook_response(response_text)
+@app.route('/dialogflow', methods=['POST'])
+def dialogflow():
+  data = request.get_json()
 
-# Função para criar a resposta no formato aceito pelo Dialogflow
-def make_webhook_response(text):
-    return jsonify({
-        "fulfillmentText": text,
-        "source": "webhook"
-    })
+  action = data['queryResult']['action']
+
+  if action == 'defaultWelcomeIntent':
+    response = format_response(['Hi, how can I help you today?'])
+  elif action == 'input.welcome':
+    response = format_response(['testando resposta','apareceu aii?'])
+  elif action == 'inputUnknown':
+    response = format_response(['Sorry, I did not understand that clearly.'])
+  else:
+    response = format_response([f'No handler for the action name {action}.'])
+  return response
 
 if __name__ == '__main__':
     # Pegar a porta da variável de ambiente ou usar 5000 como padrão
